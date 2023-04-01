524163b70aba117e5c854f250dd1ba53b71b35e4
Leonid Skurikhin <leonid.skurihin@yandex.ru>
2023-04-01 10:14:43 +0300

    Add index page

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+    <div class="managment"></div>
+  </main>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+.managment {
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  height: 97px;
+}
+</style>
