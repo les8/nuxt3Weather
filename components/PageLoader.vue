b00e9e8dfc041df7f37bbbac7f9dc407ae13b0b7
Leonid Skurikhin <leonid.skurihin@yandex.ru>
2023-04-29 21:08:51 +0300

    Лоадер

---
+++ components/PageLoader.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="loader" />
+</template>
+
+<script setup>
+import { useWeatherStore } from '~/stores/WeatherStore';
+import { onMounted } from 'vue';
+
+const weatherStore = useWeatherStore();
+
+onMounted(() => {
+  weatherStore.setWeatherByCoords()
+});
+</script>
