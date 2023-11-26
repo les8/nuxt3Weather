483deee4b04f377339f35f222ffa2907fa354e66
Leonid Skurikhin <leonid.skurihin@yandex.ru>
2023-11-26 14:49:37 +0300

    Add remove user route

---
+++ server/api/user/remove.ts
@@ -0,0 +1,40 @@
+import { prisma } from '~/prisma/prisma-client';
+
+export default defineEventHandler(async (event) => {
+  /**
+  * @route DELETE /api/user/remove
+  * @desс Удаление аккаунта
+  * @access Private
+  */
+
+  if (event.node.req.method !== 'DELETE') {
+    return {
+      status: 405,
+      message: `Method ${event.node.req.method} not allowed`
+    }
+  }  
+
+  try {
+    Promise.all([
+      prisma.favoritesCities.deleteMany({
+        where: {
+          userId: event.context.auth.user.id,
+        }
+      }),
+      prisma.user.delete({
+        where: {
+          id: event.context.auth.user.id,
+        }
+      })
+    ])
+
+    return {
+      status: 204
+    }
+  } catch (error) {
+    return {
+      status: 500,
+      message: 'Something went wrong'
+    }
+  }
+})
