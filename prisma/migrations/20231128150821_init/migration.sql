e3b5f57aec68940eea44d1e6a370515c162d0c1b
Leonid Skurikhin <leonid.skurihin@yandex.ru>
2023-11-28 18:16:04 +0300

    Переписал миграции и ДБ

--- prisma/migrations/20231128150821_init/migration.sql
+++ prisma/migrations/20231128150821_init/migration.sql
@@ -4,7 +4,17 @@ CREATE TABLE "User" (
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "name" TEXT NOT NULL,
-    "openWeatherKey" TEXT NOT NULL
+    "openWeatherKey" TEXT
+);
+
+-- CreateTable
+CREATE TABLE "Settings" (
+    "temperature" TEXT NOT NULL,
+    "wind" TEXT NOT NULL,
+    "pressure" TEXT NOT NULL,
+    "visibility" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    CONSTRAINT "Settings_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 -- CreateTable
@@ -14,3 +24,6 @@ CREATE TABLE "FavoritesCities" (
     "userId" TEXT NOT NULL,
     CONSTRAINT "FavoritesCities_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Settings_userId_key" ON "Settings"("userId");
